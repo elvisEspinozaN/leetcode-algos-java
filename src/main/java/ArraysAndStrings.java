2be1ab1841a6bc645cb01508b1815adb56898895
Elvis Espinoza <elvis.espinoza.navarrete@outlook.com>
2023-01-14 17:53:26 -0500

    squares of sorted arr, two pointer approach to sort

--- src/main/java/ArraysAndStrings.java
+++ src/main/java/ArraysAndStrings.java
@@ -29,4 +29,38 @@ public class ArraysAndStrings {
 
     }
 
+    /* Squares of a Sorted Array
+
+    Given an integer array nums sorted in non-decreasing order, return an array of the
+    squares of each number sorted in non-decreasing order.
+
+    Example 1 :
+    Input: nums = [-4,-1,0,3,10]
+    Output: [0,1,9,16,100]
+    Explanation: After squaring, the array becomes [16,1,0,9,100].
+    After sorting, it becomes [0,1,9,16,100].
+     */
+
+    public int[] sortedSquares(int[] nums) {
+        int right = nums.length - 1;
+        int left = 0;
+        int[] answer = new int[nums.length];
+
+        for(int i = nums.length - 1; i >= 0; i--) {
+            int square;
+
+            if(Math.abs(nums[left]) > Math.abs(nums[right])) {
+                square = nums[left];
+                left++;
+            } else {
+                square = nums[right];
+                right--;
+            }
+
+            answer[i] = square * square;
+        }
+
+        return answer;
+    }
+
 }
