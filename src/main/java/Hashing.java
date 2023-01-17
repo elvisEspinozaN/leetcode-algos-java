665794ca676ab9e2065b9b8ebc020325da9cec2d
Elvis Espinoza <elvis.espinoza.navarrete@outlook.com>
2023-01-17 15:58:14 -0500

    missing Number, use Hash Set to find missing number

--- src/main/java/Hashing.java
+++ src/main/java/Hashing.java
@@ -89,4 +89,32 @@ public class Hashing {
         return hs.size() == 26; // size adds up to 26
     }
 
+    /* Missing Number
+
+    Given an array nums containing n distinct numbers in the range [0, n],
+    return the only number in the range that is missing from the array.
+
+    Example :
+    Input: nums = [3,0,1]
+    Output: 2
+    Explanation: n = 3 since there are 3 numbers, so all numbers are in the range [0,3].
+    2 is the missing number in the range since it does not appear in nums.
+    */
+
+    public int missingNumber(int[] nums) {
+
+        Set<Integer> hs = new HashSet<>();
+
+        for(int i : nums) {
+            hs.add(i);
+        }
+
+        for(int i = 0; i < nums.length + 1; i++) {
+            if(!hs.contains(i)) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
 }
