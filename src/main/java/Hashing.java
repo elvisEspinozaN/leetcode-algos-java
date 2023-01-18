82614be7057c71d42b87fb6c6887a70f807c1f6a
Elvis Espinoza <elvis.espinoza.navarrete@outlook.com>
2023-01-17 19:38:34 -0500

    are Occurences Equal, use Hash Map to appoint apperances, Hash Sets to handle duplicates

--- src/main/java/Hashing.java
+++ src/main/java/Hashing.java
@@ -181,7 +181,7 @@ public class Hashing {
         return answer;
     }
 
-    /* Example 2: 2248. Intersection of Multiple Arrays
+    /* Example 2248 : Intersection of Multiple Arrays
 
     Given a 2D array nums that contains n arrays of distinct integers, return a sorted
     array containing all the numbers that appear in all n arrays.
@@ -209,6 +209,26 @@ public class Hashing {
 
         Collections.sort(answer);
         return answer;
+    }
+
+    /* Example 1941 : Check if All Characters Have Equal Number of Occurrences
+
+    Given a string s, determine if all characters have the same frequency.
+
+    For example, given s = "abacbc", return true. All characters appear twice.
+    Given s = "aaabb", return false. "a" appears 3 times, "b" appears 2 times. 3 != 2.
+     */
+
+    public boolean areOccurencesEqual(String s) {
+
+        Map<Character, Integer> map = new HashMap<>();
+
+        for(char c : s.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+
+        Set<Integer> frequencies = new HashSet<>(map.values());
+        return frequencies.size() == 1;
     }
 
 }
